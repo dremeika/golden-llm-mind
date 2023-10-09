cac4aac65c56bd59b5b8cc342834cb0acde8377a
Donatas Remeika <donatas.remeika@gmail.com>
2023-10-09 22:02:12 +0300

    Round 1 parser

--- game/episode_parser.py
+++ game/episode_parser.py
@@ -13,11 +13,28 @@ def parse_url(text: str) -> str:
 
 def parse_round1(text: str) -> list[R1Question]:
 
-    round1_match = re.match(r".+I turas\n(.*)\nII turas.+", text, re.DOTALL)
+    round1_pattern = r".+I turas\n(.*)\nII turas.+"
+    round1_match = re.match(round1_pattern, text, re.DOTALL)
     if round1_match is None:
         raise ValueError("Round 1 not found")
     round1_text = round1_match.group(1)
-    return []
+
+    block_pattern = r"(K: .+?A: .+?)\n\n"
+    block_matches = re.findall(block_pattern, round1_text, re.DOTALL)
+
+    result = []
+    for block in block_matches:
+        question_pattern = r"K:\s*(.+?)\n"
+        options_pattern = r"\w\)\s*(.+?)\n"
+        answer_pattern = r"A:\s*(.+?)$"
+
+        question = re.search(question_pattern, block).group(1)
+        options = re.findall(options_pattern, block)
+        answer = re.search(answer_pattern, block).group(1)
+
+        result.append(R1Question(question, options, answer))
+    return result
+
 
 def parse_round2(text: str) -> list[R2Question]:
 
@@ -35,6 +52,7 @@ def parse_round3(text: str) -> list[R3Question]:
         raise ValueError("Round 3 not found")
     round3_text = round3_match.group(1)
     return []
+
 
 def parse_round4(text: str) -> list[R4Question]:
 
